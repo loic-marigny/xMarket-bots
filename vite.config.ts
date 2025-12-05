9ae60ec333314ebf6a5c1b2506aed4b95d549cf3
loic-marigny <loic.marigny@edu.devinci.fr>
2025-12-05 15:52:12 +0200

    Update routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/bot-canvas-dash/" : "/",
+  base: mode === "production" ? "/xMarket-bots/" : "/",
   server: {
     host: "::",
     port: 8080,
